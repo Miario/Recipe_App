bcd14eb98e9719a3f82dd3264c04ed352260ceb7
Miario <mario.indyushkin@gmail.com>
2018-06-19 11:56:21 -0700

    adding Recipe List component

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
 import Recipe from './Recipe';
 import Navbar from './Navbar';
+import RecipeList from './RecipeList';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
-  static defaultProps = {
-    recipes: [{
-      title: "Spaghetti",
-      ingredients: ["flour", "water"],
-      instructions: "Mix ingredients",
-      img: "spaghetti.jpg"
-    }]
-  }
   render() {
     return (
       <div className="App">
         <Navbar />
-        {this.props.recipes.map((r, index) => (
-          <Recipe key={index} 
-                  title={r.title}
-                  ingredients={r.ingredients}
-                  img={r.img}
-                  instructions={r.instructions}
-          />
-        ))}
+        <RecipeList />
       </div>
     );
   }
